6664c2f634466579f9e1aecd2c6d7147a17e94cb
Burak Öner <info@burakoner.com>
2024-03-07 13:30:32 +0300

    Version 1.3.5

--- src/TcpSharp/Events/OnClientDataReceivedEventArgs.cs
+++ src/TcpSharp/Events/OnClientDataReceivedEventArgs.cs
@@ -1,6 +1,28 @@
 ﻿namespace TcpSharp;
 
-public class OnClientDataReceivedEventArgs : EventArgs
+public class OnClientDataReceivedEventArgs : EventArgs, IDisposable
 {
     public byte[] Data { get; internal set; }
+
+    ~OnClientDataReceivedEventArgs()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            // Free any other managed objects here.
+            Data = null;
+        }
+
+        // Free any unmanaged objects here.
+    }
 }
--- src/TcpSharp/Events/OnServerDataReceivedEventArgs.cs
+++ src/TcpSharp/Events/OnServerDataReceivedEventArgs.cs
@@ -1,8 +1,31 @@
 ﻿namespace TcpSharp;
 
-public class OnServerDataReceivedEventArgs : EventArgs
+public class OnServerDataReceivedEventArgs : EventArgs, IDisposable
 {
     public TcpClient Client { get; internal set; }
     public string ConnectionId { get; internal set; }
     public byte[] Data { get; internal set; }
+
+    ~OnServerDataReceivedEventArgs()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            // Free any other managed objects here.
+            Client?.Dispose();
+            Data = null;
+        }
+
+        // Free any unmanaged objects here.
+    }
 }
